4d64fb4d41cc6a7e8c0676e2b7e1e28c7f50b7e0
joashhsaoj <j5o6a2s7h4@gmail.com>
2024-12-28 10:00:53 +0800

    2024-12-28 A8

--- src/app/clientcomponent/clientcomponent.tsx
+++ src/app/clientcomponent/clientcomponent.tsx
@@ -7,10 +7,17 @@ function ClientComponent() {
     // const handleMessage = ;
 
     window.addEventListener("message", (event: MessageEvent) => {
-      if (event.origin === "http://v1.web1v1.cn") {
-        const { name, gender, age, location } = event.data;
+      window.console.log("hello");
+      window.console.log("Event origin:", event.origin);
+      window.console.log("Event data:", event.data);
+
+      if (event.origin === "http://v1.web1v1.cn/randomdeskrynew.html") {
+        window.console.log("Origin matched");
+
         if (event.data) {
-          console.log(event.data);
+          const { name, gender, age, location } = event.data;
+          console.log("Sending data:", { name, gender, age, location });
+
           // 发送请求到 API 路由
           fetch("/api/test", {
             method: "POST",
@@ -24,7 +31,17 @@ function ClientComponent() {
               location: location,
             }),
           })
-            .then((response) => response.json())
+            .then((response) => {
+              console.log("Response status:", response.status);
+
+              // 检查响应状态
+              if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+              }
+
+              // 解析 JSON
+              return response.json();
+            })
             .then((data) => {
               console.log("Data added:", data);
             })
@@ -34,6 +51,7 @@ function ClientComponent() {
         }
       }
     });
+
     // // 清理事件监听器
     // return () => {
     //   window.removeEventListener("message", handleMessage);
