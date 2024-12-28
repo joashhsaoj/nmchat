b49a558803a4bd6136b42fba3b7aac4223d61ced
joashhsaoj <j5o6a2s7h4@gmail.com>
2024-12-28 10:24:49 +0800

    2024-12-28 A10

--- src/app/clientcomponent/clientcomponent.tsx
+++ src/app/clientcomponent/clientcomponent.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { useEffect } from "react";
 
 function ClientComponent() {
@@ -8,12 +6,9 @@ function ClientComponent() {
 
     window.addEventListener("message", (event: MessageEvent) => {
       if (event.origin === "http://v1.web1v1.cn") {
-        window.console.log("Origin matched");
-
+        const { name, gender, age, location } = event.data;
         if (event.data) {
-          const { name, gender, age, location } = event.data;
-          console.log("Sending data:", { name, gender, age, location });
-
+          console.log(event.data);
           // 发送请求到 API 路由
           fetch("/api/test", {
             method: "POST",
@@ -27,25 +22,17 @@ function ClientComponent() {
               location: location,
             }),
           })
-            .then((response) => {
-              console.log("Response status:", response.status);
-              // 检查响应状态
-              if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-              }
-              // 解析 JSON
-              return response.json();
-            })
+            .then((response) => response.json())
             .then((data) => {
               console.log("Data added:", data);
             })
             .catch((error) => {
               console.error("Error adding data:", error);
             });
+          console.log(name, gender, age, location);
         }
       }
     });
-
     // // 清理事件监听器
     // return () => {
     //   window.removeEventListener("message", handleMessage);
